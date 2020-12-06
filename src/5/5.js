4c444ef41cbfe5a59f5678909d870a8da371921d
Alex Rothuis <alex.rothuis@gmail.com>
2020-12-07 00:10:09 +0100

    Use alternative for day 5 and don't count alts for LOC

--- src/5/5.js
+++ src/5/5.js
@@ -1,40 +1,13 @@
 const { linesFromFile } = require("../core");
 
-const halfRange = (a, b) => Math.round((b - a) / 2);
+const boardingPass = pass => 
+    parseInt(pass.replace(/[FL]/g, "0").replace(/[BR]/g, "1"), 2);
 
-const lowerHalf = ([a, b]) => [a, b - halfRange(a, b)];
-const upperHalf = ([a, b]) => [a + halfRange(a, b), b];
-
-const parseRow = (range, letter) => 
-    letter === "F" ? lowerHalf(range) : upperHalf(range);
-const parseColumn = (range, letter) =>
-    letter === "R" ? upperHalf(range) : lowerHalf(range);
-
-const findSeatRow = p => p.split("").slice(0, 7).reduce(parseRow, [0, 127])[0];
-const findSeatColumn = p => p.split("").slice(7).reduce(parseColumn, [0, 7])[0]
-
-const boardingPass = p => (
-    row = findSeatRow(p), 
-    column = findSeatColumn(p), 
-    id = row * 8 + column,
-    [row, column, id]
-);
-
-const solveA = path => 
-    linesFromFile(path)
-        .map(x => boardingPass(x)[2])
-        .reduce((max, id) => id > max ? id : max, 0);
-
-const solveB = path => 
-    linesFromFile(path)
-        .map(x => boardingPass(x)[2])
-        .sort((a, b) => a - b)
-        .filter((id, i, ids) => ids[i + 1] !== id + 1)[0] 
-        + 1
+const solveA = path => linesFromFile(path).map(boardingPass).reduce((a, b) => Math.max(a, b), 0);
+const solveB = path => linesFromFile(path).map(boardingPass).sort((a, b) => a - b)
+    .filter((id, i, ids) => ids[i + 1] !== id + 1)[0] + 1;
 
 module.exports =  {
-    lowerHalf,
-    upperHalf,
     boardingPass,
     solveA,
     solveB,
--- src/5/5.test.js
+++ src/5/5.test.js
@@ -1,37 +1,14 @@
 const { expect } = require("chai");
-const { solveA, solveB, lowerHalf, upperHalf, findSeatRow, findSeatColumn, boardingPass } = require("./5");
+const { solveA, solveB, boardingPass } = require("./5");
 
-describe("Day 5", function () {
+describe("Day 5 (alternative)", function () {
     context("helpers", function () {
-        specify("lower half", function () {
-            const examples = [
-                [[0, 127], [0, 63]],
-                [[32, 63], [32, 47]],
-                [[4, 7], [4, 5]],
-                [[44, 47], [44, 45]],
-                [[44, 45], [44, 44]],
-            ];
-            examples.forEach(([start, expected]) => {
-                expect(lowerHalf(start)).to.deep.equal(expected);
-            });
-        });
-        
-        specify("upper half", function () {
-            const examples = [
-                [[0, 63], [32, 63]],
-                [[32, 47], [40, 47]],
-            ];
-            examples.forEach(([start, expected]) => {
-                expect(upperHalf(start)).to.deep.equal(expected);
-            });
-        });
-
         specify("boarding pass", function () {
             const examples = [
-                ["FBFBBFFRLR", [44, 5, 357]],
-                ["BFFFBBFRRR", [70, 7, 567]],
-                ["FFFBBBFRRR", [14, 7, 119]],
-                ["BBFFBBFRLL", [102, 4, 820]],
+                ["FBFBBFFRLR", 357],
+                ["BFFFBBFRRR", 567],
+                ["FFFBBBFRRR", 119],
+                ["BBFFBBFRLL", 820],
             ];
             examples.forEach(([pass, expected]) => {
                 expect(boardingPass(pass)).to.deep.equal(expected);
@@ -41,7 +18,7 @@ describe("Day 5", function () {
 
     context("A", function () {
         specify("solution", function () {
-            const result = solveA(`${__dirname}/input.txt`);
+            const result = solveA(`${__dirname}/../input.txt`);
             console.log(result);
             expect(result).to.equal(974);
         });
@@ -49,7 +26,7 @@ describe("Day 5", function () {
 
     context("B", function () {
         specify("solution", function () {
-            const result = solveB(`${__dirname}/input.txt`);
+            const result = solveB(`${__dirname}/../input.txt`);
             console.log(result);
             expect(result).to.equal(646);
         });
