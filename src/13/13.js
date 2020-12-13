a1c01804dbbbfd04ce8debce45d53a7444cfd9e5
Alex Rothuis <alex.rothuis@gmail.com>
2020-12-13 17:48:06 +0100

    Refactor to recursion
    Not sure if I like it this way, just wanted to see how it could be done

--- src/13/13.js
+++ src/13/13.js
@@ -3,29 +3,21 @@ const { linesFromFile } = require("../core.js");
 const loadBuses = path => {
     const [time, data] = linesFromFile(path);
     return data.split(",")
-        .map((b, i) => ({ i, offset: b - time % +b, value: +b }))
-        .filter(({value}) => !isNaN(value));
+        .map((b, i) => ({ i, offset: b - time % +b, id: +b }))
+        .filter(({id}) => !isNaN(id));
 };
+const findBusSequenceTime = ([bus, ...buses], stepSize, time = 0) => 
+    bus === undefined ? time : 
+    (time + bus.i) % bus.id === 0 ? findBusSequenceTime(buses, stepSize * bus.id, time)
+        : findBusSequenceTime([bus, ...buses], stepSize, time + stepSize);
 
 const solveA = path => {
     const first = loadBuses(path).sort((a, b) => a.offset - b.offset)[0];
-    return first.offset * first.value;
+    return first.offset * first.id;
 };
-
 const solveB = path => {
     const [initial, ...buses] = loadBuses(path);
-        
-    let stepSize = initial.value;
-    let time = 0;
-
-    for (let {i, value} of buses) {
-        while ((time + i) % value !== 0) {
-            time += stepSize;
-        }
-        stepSize *= value;
-    }
-     
-    return time;
+    return findBusSequenceTime(buses, initial.id);
 };
 
 module.exports =  {
