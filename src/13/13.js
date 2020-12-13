a6d8f2423a8a5562b948fb7297ca9cb34658c915
Alex Rothuis <alex.rothuis@gmail.com>
2020-12-13 17:25:51 +0100

    Extract commonalities

--- src/13/13.js
+++ src/13/13.js
@@ -1,29 +1,28 @@
 const { linesFromFile } = require("../core.js");
 
-const solveA = path => {
-    const [time, buses] = linesFromFile(path);
-    const [wait, bus] = buses.split(",")
-        .filter(b => b !== "x")
-        .map(b => [b - time % +b, +b])
-        .sort((a, b) => a[0] - b[0])[0];
-    return wait * bus;
+const loadBuses = path => {
+    const [time, data] = linesFromFile(path);
+    return data.split(",")
+        .map((b, i) => ({ i, offset: b - time % +b, value: +b }))
+        .filter(({value}) => !isNaN(value));
 };
 
-// TODO: Refactor to more functional approach (recursion / fold)
-// TODO: Dive into Chinese Remainder Theory
+const solveA = path => {
+    const first = loadBuses(path).sort((a, b) => a.offset - b.offset)[0];
+    return first.offset * first.value;
+};
+
 const solveB = path => {
-    const buses = linesFromFile(path)[1].split(",")
-        .map((n, i) => [i, +n])
-        .filter(b => !isNaN(b[1]));
+    const [initial, ...buses] = loadBuses(path);
         
-    let stepSize = buses[0][1];
+    let stepSize = initial.value;
     let time = 0;
 
-    for (let [i, n] of buses.slice(1)) {
-        while ((time + i) % n !== 0) {
+    for (let {i, value} of buses) {
+        while ((time + i) % value !== 0) {
             time += stepSize;
         }
-        stepSize *= n;
+        stepSize *= value;
     }
      
     return time;
