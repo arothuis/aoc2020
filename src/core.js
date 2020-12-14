d8f0d1958a832d30651ae241dcbc1a0dc0739d14
Alex Rothuis <alex.rothuis@gmail.com>
2020-12-14 22:10:41 +0100

    Calculate powerset

--- src/core.js
+++ src/core.js
@@ -7,8 +7,20 @@ const numbersFromFile = (path, delim = "\n", base = 10) =>
 
 const combinations = xs => xs.flatMap(x => xs.map(y => [x, y]));
 
+const powerset = xs => {
+    const ys = [[]];
+    for (let i = 0; i < xs.length; i++) {
+        for (let j = 0, len = ys.length; j < len; j++) {
+            ys.push(ys[j].concat(xs[i]));
+        }
+    }
+    return ys;
+};
+
+
 module.exports = {
     linesFromFile,
     numbersFromFile,
     combinations,
+    powerset,
 };
